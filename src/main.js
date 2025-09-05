fae74522ba930134d22be7deadb3ed14f97c9b7b
Andrey <kenjo@jlab.org>
2025-09-05 15:54:20 -0400

    initial pages commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import "./assets/styles/main.css";
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
+import { definePreset } from '@primeuix/themes'
+import Nora from "@primeuix/themes/nora"
+
+const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Nora,
+        options: {
+            prefix: 'p',
+            darkModeSelector: '.my-app-dark',
+            cssLayer: {
+                name: 'primevue',
+                order: 'theme, base, primevue'
+            }
+        }
+    }
+})
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<!-- HelloWorld.vue -->
+<template>
+  <div class="flex">
+    <Button>Hello </Button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+</script>
+
+<style scoped>
+</style>
